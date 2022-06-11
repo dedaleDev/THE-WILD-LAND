0afd0671dd2d6a0ee416aa766d034f644b5c9113
Swigifer <77618065+Swigifer@users.noreply.github.com>
2022-06-11 19:08:55 +0200

    Add files via upload

--- tuile.py
+++ tuile.py
@@ -3,20 +3,34 @@ class Tuile(pygame.sprite.Sprite):
     def __init__(self, type):
         self.type = type
         self.canon = False
-        self.probaSup = 0
+        self.probaSup_mer = 0
+        self.probaSup_terre = 0
+        self.probaSup_foret = 0 
         
-        
+               
     def getType(self):
         return self.type
     
-    def getProba(self):
-        return self.probaSup    
+    def getProba_mer(self):
+        return self.probaSup_mer
+    
+    def getProba_roche(self):
+        return self.probaSup_roche
+
+    def getProba_foret(self):
+        return self.probaSup_foret
 
     def getCanon(self):
         return self.canon
     
 
-    def augmenterProba(self, entier):
-        self.probaSup += entier
+    def augmenterProbaMer(self, entier):
+        self.probaSup_mer += entier
+    
+    def augmenterProbaRoche(self, entier):
+        self.probaSup_Roche += entier
+        
+    def augmenterProbaForet(self, entier):
+        self.probaSup_foret += entier
     
     
