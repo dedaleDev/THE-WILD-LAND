a4a07d93e39365a92aa2cf519f89605fb7733c01
DedaleStudio <101816097+DedaleStudio@users.noreply.github.com>
2022-06-11 16:29:31 +0200

    Update options.py

--- options.py
+++ options.py
@@ -1,7 +1,8 @@
 from tkinter import *
 from tkinter import messagebox
 from PIL import Image, ImageTk
-import tkinter.font as tkFont
+import tkinter.font as font
+import main_menu
 import pygame
 fenetreOptions = ""
 
@@ -13,11 +14,11 @@ modeHD = False
 
 def init():
     global fenetreOptions, checkVar1, music
-    fenetreOptions = Toplevel(takefocus=True)
+    fenetreOptions = Tk()
     screenScale = str(fenetreOptions.winfo_screenwidth())
     screenScale += "x"
     screenScale += str(fenetreOptions.winfo_screenheight())
-    fenetreOptions.geometry(screenScale+"+0+0")
+    fenetreOptions.geometry(screenScale)
     # if music == True:
     # pygame.mixer.init()
     #mainTheme = pygame.mixer.music.load("data/Music/Options.mp3")
@@ -29,21 +30,21 @@ def init():
 def ok():
     # quand clique sur ok alors enregistrement et fermeture du menu
     global fenetreOptions, music
-    if music == True:
-        pygame.mixer.init()
-        mainTheme = pygame.mixer.music.load("data/Music/MainTheme.mp3")
-        pygame.mixer.music.play(10)
-        checkVar1 = IntVar()
     fenetreOptions.destroy()
+    fenetreOptions=""
     print("fermeture du menu option et enregistrement des modifications")
+    main_menu.Main_Menu()
     return
 
 
 def Renit():
     # fonction gérant le bouton rénitialisation
-    global isActive, fenetreOptions
+    global fenetreOptions
+    print("suppresion des sauvegardes ?")
     if messagebox.askokcancel("ATTENTION : ", "En poursuivant l'entièreté de vos sauvegardes seront effacées. Cette action est irréversible. Voulez-vous vraiment continuer ? ") == True:
         print("Ok, suppresion des sauvegardes")
+        fenetreOptions.destroy()
+        main_menu.Main_Menu()
     else:
         print("Canceled")
 
@@ -78,12 +79,10 @@ def Menu_Options():
     fenetreOptions.update_idletasks()
     fenetreOptions.title("Options")  # titre
     #fenetre.attributes('-fullscreen', True)
-    helv20 = tkFont.Font(family='Helvetica', size=20,
-                         weight='bold')  # définit la police
+    f= font.Font(size=50)
 
-    fenetreOptions.iconphoto(False, PhotoImage(
-        file="data/logo/icon_Polgarok.png"))
-    imageBGOptions = Image.open("data/Menu/menu_Option_img.jpeg")
+    #fenetreOptions.iconphoto(False, PhotoImage(file="data/logo/icon_Polgarok.png"))
+    imageBGOptions = Image.open("data/menu/main_options_background.png")
     # redimentionne la taille de l'image en fonction de la taille de l'écran
     resize_imageBGOptions = imageBGOptions.resize(
         (fenetreOptions.winfo_screenwidth(), fenetreOptions.winfo_screenheight()))
@@ -94,15 +93,13 @@ def Menu_Options():
     arrireplanLabel.pack()
     arrireplanLabel.place(x=0, y=0)
 
-    imageButtonReset = Image.open("data/Menu/button_renit.png")
-    imageButtonReset = ImageTk.PhotoImage(imageButtonReset)
-    resetSave = Button(fenetreOptions, command=Renit, image=imageButtonReset)
+    resetSave = Button(fenetreOptions, command=Renit, text= "Rénitialiser les sauvegardes")
+    resetSave['font']=f
     resetSave.pack
     resetSave.place(anchor="nw", relx=0.02, rely=0.10)
 
-    imageButtonOk = Image.open("data/Menu/button_ok.png")
-    imageButtonOk = ImageTk.PhotoImage(imageButtonOk)
-    okButton = Button(fenetreOptions, image=imageButtonOk, command=ok, )
+    okButton = Button(fenetreOptions, command=ok, text= "Ok" )
+    okButton['font'] = f
     okButton.pack
     okButton.place(anchor="nw", relx=0.02, rely=0.25)
 
