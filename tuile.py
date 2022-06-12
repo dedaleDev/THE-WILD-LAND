2feabc9f261a2dc58ce2b97c7b93396967f181d8
DedaleStudio <101816097+DedaleStudio@users.noreply.github.com>
2022-06-12 15:58:44 +0200

    Update tuile.py

--- tuile.py
+++ tuile.py
@@ -1,19 +1,20 @@
 import pygame
+
+
 class Tuile(pygame.sprite.Sprite):
     def __init__(self, type):
         self.type = type
         self.canon = False
         self.probaSup_mer = 0
         self.probaSup_roche = 0
-        self.probaSup_foret = 0 
-        
-               
+        self.probaSup_foret = 0
+
     def getType(self):
         return self.type
-    
+
     def getProba_mer(self):
         return self.probaSup_mer
-    
+
     def getProba_roche(self):
         return self.probaSup_roche
 
@@ -22,15 +23,12 @@ class Tuile(pygame.sprite.Sprite):
 
     def getCanon(self):
         return self.canon
-    
 
     def augmenterProbaMer(self, entier):
         self.probaSup_mer += entier
-    
+        print()
     def augmenterProbaRoche(self, entier):
-        self.probaSup_Roche += entier
-        
+        self.probaSup_roche += entier
+
     def augmenterProbaForet(self, entier):
         self.probaSup_foret += entier
-    
-    
