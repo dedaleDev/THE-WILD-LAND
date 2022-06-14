152a84f72f1f24475781f5117776f8fba3e34ab2
DedaleStudio <101816097+DedaleStudio@users.noreply.github.com>
2022-06-14 10:37:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import main_menu
 import pygame_gestion
 import generation
+import sys
 
 def main():
-    while main_menu.load ==True :
-        main_menu.Main_Menu() # si la partie est lancé
+    while main_menu.loadMainMenu == True :
+        if main_menu.loadMainMenu==True :
+            main_menu.Main_Menu()  # si la partie est lancé
         if main_menu.startGame == True:
-            main_menu.startGame =False
+            main_menu.loadMainMenu = False
+            main_menu.startGame = False
             print("Lancement du jeu")
             map = generation.generation_matrice()
             pygame_gestion.pygameInit(map)
+    sys.exit()
+
 
 main()
