3db9656a240e123deac1296551dc465097d36652
Swigifer <77618065+Swigifer@users.noreply.github.com>
2022-06-14 18:39:52 +0200

    Add files via upload

--- generation.py
+++ generation.py
@@ -4,70 +4,85 @@ import copy
 from tuile import Tuile
 taille_matriceX = 12#Y
 taille_matriceY = 18#X
-proba_roche = 15  # en %
-proba_mer = 5
-proba_foret = 15
-proba_desert = 10
-proba_neige= 5
+proba_roche = random.randint(10,15) # en %
+proba_mer = random.randint(3,15)
+proba_desert = random.randint(5,15)
+if proba_mer<=6:
+    proba_desert+=5
+proba_foret = random.randint(10,15)
+proba_neige= 0
+
 
 nombre_biome = 5
-
-
-
+GIGALISTE=[]
+liste_index = []
 
 def tirer_mer(matriceMap, i, j):
     temp = random.randint(1, 100)
-    if temp <= proba_mer + matriceMap[i][j].getProba_mer():
+    if temp <= proba_mer + matriceMap[i][j].getProbaMer():
         matriceMap[i][j] = Tuile(3)
-        majProba(matriceMap, i, j, 100, "mer")
+        majProba(matriceMap, i, j, 70, "mer")
+        return True
+    return False
 
 
 def tirer_desert(matriceMap, i, j):
     temp = random.randint(1, 100)
-    if temp <= proba_desert + matriceMap[i][j].getProba_desert():
+    if temp <= proba_desert + matriceMap[i][j].getProbaDesert() and matriceMap[i][j].getAutoriserDesert():
         matriceMap[i][j] = Tuile(6)
-        majProba(matriceMap, i, j, 100, "desert")
-        majProba(matriceMap, i, j, -100, "neige")
+        majProba(matriceMap, i, j, 60, "desert")
+        return True
+    return False
+        
 
 def tirer_foret(matriceMap, i, j):
     temp = random.randint(1, 100)
-    if temp <= proba_foret + matriceMap[i][j].getProba_foret():
+    if temp <= proba_foret + matriceMap[i][j].getProbaForet():
         matriceMap[i][j] = Tuile(4)
-        majProba(matriceMap, i, j, 50, "foret")
+        majProba(matriceMap, i, j, 30, "foret")
+        return True
+    return False
 
 
-def tirer_neige(matriceMap, i, j):
+def tirer_neige(matriceMap, i, j) :
     temp = random.randint(1, 100)
-    if temp <= proba_neige + matriceMap[i][j].getProba_neige():
+    if temp <= proba_neige + matriceMap[i][j].getProbaNeige() and matriceMap[i][j].getAutoriserNeige():
         matriceMap[i][j] = Tuile(5)
-        #majProba(matriceMap, i, j, 25, "neige")
-        majProba(matriceMap, i, j, -100, "desert")
+        majNeigeAutoriser(matriceMap, i,j)
+        majProba(matriceMap, i, j, 15, "neige")
+        majDesertInterdire(matriceMap, i,j)
+        return True
+    return False
 
 def tirer_roche(matriceMap, i, j):
     temp = random.randint(1, 100)
-    if temp <= proba_roche + matriceMap[i][j].getProba_roche():
+    if temp <= proba_roche + matriceMap[i][j].getProbaRoche():
         matriceMap[i][j] = Tuile(2)
         majProba(matriceMap, i, j, 25, nature= "roche")
-        majProba(matriceMap, i, j, 100, nature= "neige")
-        majProba(matriceMap, i, j, -100, "desert")
+        majNeigeAutoriser(matriceMap, i,j)
+        majDesertInterdire(matriceMap, i,j)
+        majProba(matriceMap, i,j,25, "neige")
+        return True
+    return False
+        
 
 def tirer_biome(type, matriceMap, i, j):
     if type==2:
-        tirer_roche(matriceMap, i, j)
+        tire=tirer_roche(matriceMap, i, j)
     elif type == 3:
-        tirer_mer(matriceMap, i, j)
+        tire=tirer_mer(matriceMap, i, j)
     elif type == 4:
-        tirer_foret(matriceMap, i, j)
+        tire=tirer_foret(matriceMap, i, j)
     elif type == 5:
-        tirer_neige(matriceMap, i, j)
+        tire=tirer_neige(matriceMap, i, j)
     elif type == 6:
-        tirer_desert(matriceMap, i, j)
+        tire=tirer_desert(matriceMap, i, j)
+    return tire
 
 def majProba(matriceMap, i, j, probaSup, nature):
-    liste =[-1,1]
-    for x in range(-1, 1):
-        for y in range(-1, 1):
-            if i+x < len(matriceMap) and j+y < len(matriceMap[0]):
+    for x in range(-1, 2):
+        for y in range(-1, 2):
+            if i+x < len(matriceMap) and j+y < len(matriceMap[0])and i+x>= 0 and y+j>=0:
                 if nature == "roche":
                     matriceMap[i+x][j+y].augmenterProbaRoche(probaSup)
                 if nature == "foret":
@@ -80,6 +95,24 @@ def majProba(matriceMap, i, j, probaSup, nature):
                     matriceMap[i+x][j+y].augmenterProbaNeige(probaSup)
 
 
+def majNeigeAutoriser(matriceMap, i, j):
+    for x in range(-1, 2):
+        for y in range(-1, 2):
+            if i+x < len(matriceMap) and j+y < len(matriceMap[0]) and i+x>= 0 and y+j>=0:
+                matriceMap[i+x][j+y].setAutoriserNeige()
+
+
+def majDesertInterdire(matriceMap, i, j):
+    for x in range(-1, 2):
+        for y in range(-1, 2):
+            if i+x < len(matriceMap) and i+x >=0 and j+y < len(matriceMap[0]) and j+y >= 0:
+                
+                matriceMap[i+x][j+y].setInterdireDesert()
+                #printMat(matriceMap)
+                if matriceMap[i+x][j+y].getType()==6:
+                    matriceMap[i+x][j+y].setType(1)
+
+
 def generation_matrice():
     #1 = terre
     #2 = roche
@@ -89,7 +122,6 @@ def generation_matrice():
     #6 = desert
     
     
-    liste_index = []
     for i in range(1, taille_matriceX-1):
         for j in range(1, taille_matriceY-1):
             liste_index.append((i,j))
@@ -110,9 +142,9 @@ def generation_matrice():
         matriceMap[0][i] = Tuile(7)
         matriceMap[taille_matriceX-1][i] = Tuile(7)
 
+
     
-    
-    while len(liste_index) != 0: 
+    while len(liste_index) != 0:
         num_case = random.randint(0, len(liste_index)-1)
         
         i=liste_index[num_case][0]
@@ -122,19 +154,21 @@ def generation_matrice():
         
         liste_type = [2,3,4,5,6]
         
-        while len(liste_type)!=0: 
+        Tire=False
+        while len(liste_type)!=0 and not Tire:
             biome = random.randint(0, len(liste_type)-1)
-            tirer_biome(liste_type[biome], matriceMap, i, j)
+            Tire = tirer_biome(liste_type[biome], matriceMap, i, j)
             liste_type.pop(biome)
-            print(liste_type)
-    printMat(matriceMap)
+        #GIGALISTE.append(copy.deepcopy(matriceMap))
+    
+
     return matriceMap
 
 def printMat(matriceMap):
     for i in range(len(matriceMap)):
         print("\n")
         for j in range(len(matriceMap[0])):
-            print(matriceMap[i][j].getProba_desert(),", ", end='')
+            print(matriceMap[i][j].getAutoriserDesert(),',', end='')
 
 def loadImg(matriceMap=[], matricemap=[]):
     #fonction pour remplacer le plan de la map par des images
--- tuile.py
+++ tuile.py
@@ -10,30 +10,53 @@ class Tuile(pygame.sprite.Sprite):
         self.probaSup_foret = 0
         self.probaSup_desert = 0
         self.probaSup_neige = 0
+        self.autoriserNeige = False
+        self.autoriserDesert = True
 
+
+    def setType(self, entier):
+        self.type=entier
+    
+    
     def getType(self):
         return self.type
 
-    def getProba_mer(self):
+    def getProbaMer(self):
         return self.probaSup_mer
-    def getProba_desert(self):
+    
+    def getProbaDesert(self):
         return self.probaSup_desert
 
-    def getProba_roche(self):
+    def getProbaRoche(self):
         return self.probaSup_roche
 
-    def getProba_neige(self):
+    def getProbaNeige(self):
         return self.probaSup_neige
 
-    def getProba_foret(self):
+    def getProbaForet(self):
         return self.probaSup_foret
 
     def getCanon(self):
         return self.canon
 
+    def getAutoriserNeige(self):
+        return self.autoriserNeige
+    
+    def getAutoriserDesert(self):
+        return self.autoriserDesert
+    
+    def setInterdireDesert(self):
+        self.autoriserDesert = False
+    
+    def setAutoriserNeige(self):
+        self.autoriserNeige = True
+    
+    def interdireNeige(self):
+        self.autoriserNeige = False
+    
     def augmenterProbaMer(self, entier):
         self.probaSup_mer += entier
-        print()
+
     def augmenterProbaRoche(self, entier):
         self.probaSup_roche += entier
 
