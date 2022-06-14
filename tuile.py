3471d53ffaa6f212dbc1cfe83e936c33fccd7db8
DedaleStudio <101816097+DedaleStudio@users.noreply.github.com>
2022-06-14 10:36:59 +0200

    Update tuile.py

--- tuile.py
+++ tuile.py
@@ -8,15 +8,22 @@ class Tuile(pygame.sprite.Sprite):
         self.probaSup_mer = 0
         self.probaSup_roche = 0
         self.probaSup_foret = 0
+        self.probaSup_desert = 0
+        self.probaSup_neige = 0
 
     def getType(self):
         return self.type
 
     def getProba_mer(self):
         return self.probaSup_mer
+    def getProba_desert(self):
+        return self.probaSup_desert
 
     def getProba_roche(self):
         return self.probaSup_roche
+
+    def getProba_neige(self):
+        return self.probaSup_neige
 
     def getProba_foret(self):
         return self.probaSup_foret
@@ -32,3 +39,7 @@ class Tuile(pygame.sprite.Sprite):
 
     def augmenterProbaForet(self, entier):
         self.probaSup_foret += entier
+    def augmenterProbaDesert(self, entier):
+        self.probaSup_desert += entier
+    def augmenterProbaNeige(self, entier):
+        self.probaSup_neige += entier
