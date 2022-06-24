723d9c1e0d9dcef3c5c4923f184481d8f75dd7a5
DedaleStudio <101816097+DedaleStudio@users.noreply.github.com>
2022-06-24 22:44:21 +0200

    Update joueur.py

--- joueur.py
+++ joueur.py
@@ -9,6 +9,8 @@ class Player(pygame.sprite.Sprite):
           self.skin = self.loadSkin("joueur_1")
           self.game = game
 
+
+
           #donnée du joueur
           self.health = 100
           self.velocity = 5
@@ -17,10 +19,12 @@ class Player(pygame.sprite.Sprite):
           self.posY=5
 
           #ressources du joueur
-          self.bois = 0
-          self.pierre = 0
-          self.nourriture = 0
-          self.eau = 0
+          self.wood = 200
+          self.stone = 100
+          self.food = 50
+          self.water = 50
+          self.RessourcesTEXT =""
+          self.ressourcesIMG = self.loadRessourcesIMG()
 
 
      def getSkin(self):
@@ -34,14 +38,14 @@ class Player(pygame.sprite.Sprite):
      def getArmor(self): 
                return self.armor
 
-     def getBois(self):
-           return self.bois
-     def getPierre(self):
-               return self.pierre
-     def getNourriture(self):
-               return self.nourriture
-     def getEau(self):
-               return self.eau
+     def getWood(self):
+           return self.wood
+     def getStone(self):
+               return self.stone
+     def getFood(self):
+               return self.food
+     def getWater(self):
+               return self.water
 
      def setHealth(self, health):
                self.health += health     
@@ -50,14 +54,18 @@ class Player(pygame.sprite.Sprite):
      def setArmor(self, armor):
                self.armor += armor
                
-     def setBois(self, bois):
-               self.bois += bois
-     def setPierre(self, pierre):
-               self.pierre += pierre
-     def setNourriture(self, nourriture):
-               self.nourriture += nourriture
-     def setEau(self, eau):
-               self.eau += eau
+     def setWood(self, wood):
+               self.wood += wood
+               self.compteurRessources()
+     def setStone(self, stone):
+               self.stone += stone
+               self.compteurRessources()
+     def setFood(self, food):
+               self.food += food
+               self.compteurRessources()
+     def setWater(self, water):
+               self.water += water
+               self.compteurRessources()
      def loadSkin(self, skin):
         self.skin = pygame.image.load("data/personnages/"+skin+".png")
         self.skin = pygame.transform.scale(self.skin, (100, 150))
@@ -66,10 +74,6 @@ class Player(pygame.sprite.Sprite):
     
     
      def deplacementAutorise(self, direction):
-         """if self.game.map[self.posY][self.posX+1].getExplored() == False:
-            print("découverte d'une nouvelle tuile...")
-            self.game.deleteFog(self.posX, self.posY+1)
-            self.game.generateFog()"""
          if direction=="droite":
             return not (self.game.map[self.posY][self.posX+1].estMontagne() or self.game.map[self.posY][self.posX+1].estMer()) #on ne doit pas avoir mer ou montagne
          if direction=="gauche":
@@ -78,7 +82,15 @@ class Player(pygame.sprite.Sprite):
              return not (self.game.map[self.posY-1][self.posX].estMontagne() or self.game.map[self.posY-1][self.posX].estMer())
          if direction=="bas":
              return not (self.game.map[self.posY+1][self.posX].estMontagne() or self.game.map[self.posY+1][self.posX].estMer())
-     
+         if direction=="diagHautDroit":
+            return not (self.game.map[self.posY-1][self.posX+1].estMontagne() or self.game.map[self.posY-1][self.posX+1].estMer())
+         if direction=="diagHautGauche":
+            return not (self.game.map[self.posY-1][self.posX-1].estMontagne() or self.game.map[self.posY-1][self.posX-1].estMer())
+         if direction=="diagBasGauche":
+            return not (self.game.map[self.posY+1][self.posX-1].estMontagne() or self.game.map[self.posY+1][self.posX-1].estMer())
+         if direction=="diagBasDroit":
+            return not (self.game.map[self.posY+1][self.posX+1].estMontagne() or self.game.map[self.posY+1][self.posX+1].estMer())
+        
      def goLeft(self):
         self.posX-=1
         if self.posX<1:
@@ -99,3 +111,37 @@ class Player(pygame.sprite.Sprite):
          self.posY+=1
          if self.posY>len(self.game.map)-2:
             self.posY=len(self.game.map)-2
+
+     def goDownLeft(self):
+         self.goDown()
+         self.goLeft()
+
+
+     def goDownRight(self):
+         self.goDown()
+         self.goRight()
+         
+     def goUpRight(self):
+        self.goRight()
+        self.goUp()
+
+     def goUpLeft(self):
+         self.goUp()
+         self.goLeft()
+     def loadRessourcesIMG(self):
+       
+        listeRessourcesSources = ["data/ressources/r_food.png", "data/ressources/r_stone.png", "data/ressources/r_water.png", "data/ressources/r_wood.png"]
+        for i in  range (len(listeRessourcesSources)) : 
+            listeRessourcesSources[i]= pygame.image.load(listeRessourcesSources[i]).convert_alpha()
+            listeRessourcesSources[i]= pygame.transform.scale(listeRessourcesSources[i],( 170,70))
+        self.compteurRessources()
+        return listeRessourcesSources
+
+     def compteurRessources(self):
+        smallfont = pygame.font.SysFont('Corbel', 50)  # definit la police utilisé
+        listeRessources=[self.stone, self.food, self.water, self.wood]
+        listeRessourcesTEXT=["", "","",""]
+        for i in  range (len(listeRessourcesTEXT)) : 
+            listeRessourcesTEXT[i]=smallfont.render(str(listeRessources[i]), True, (0,0,0))
+        self.RessourcesTEXT= listeRessourcesTEXT
+        return
