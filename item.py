f9e6bcf533bc67b9a27a49962edd1cb7d0b5f1d6
DedaleStudio <101816097+DedaleStudio@users.noreply.github.com>
2022-06-30 18:12:07 +0200

    Update item.py

--- item.py
+++ item.py
@@ -1,13 +1,13 @@
-
 import pygame
 
 class Item(pygame.sprite.Sprite):
     
-    def __init__(self, game, nom, cout) :
+    def __init__(self, game, nom, cout, infoBulle) :
         self.game = game
         self.nom = nom
         self.image = game.images.returnImItem(nom)
         self.cout = cout
+        self.infoBulle =infoBulle
         self.rect = self.image.get_rect()
     
     
