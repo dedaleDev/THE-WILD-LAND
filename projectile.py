6c4bb30c9d938d4bbdac90775688578922a9f27f
Swigifer <77618065+Swigifer@users.noreply.github.com>
2022-07-04 17:26:26 +0200

    Add files via upload

--- projectile.py
+++ projectile.py
@@ -1,87 +1,56 @@
 import math
 import pygame
-import random
-
-from selection import majSelectionJoueur
 class Projectile(pygame.sprite.Sprite):
 
      def __init__(self, game, nom, vitesse, degat, posDepartX, posDepartY, cible):
           super().__init__()
           #affichage et information
-          self.img = game.images.loadImgProjectile(nom)
+          
           self.game = game
           self.velocity = vitesse
           self.cibleRect = cible.rect
           self.cibleX = cible.rect.x
           self.cibleY = cible.rect.y
           self.cible=cible
+          self.img=game.images.loadImgProjectile(nom)
+          
+          self.rect = self.img.get_rect()
+          self.degat = degat
+          self.angle = self.genererAngle()
+          self.img = game.images.loadImgProjectile(nom, self.angle)
+          
           self.rect = self.img.get_rect()
           self.rect.x = posDepartX
           self.rect.y = posDepartY
-          self.degat = degat
+
+     def genererAngle(self):
+        dx, dy = self.cibleX - self.rect.x, self.cibleY - self.rect.y
+        dist = math.hypot(dx, dy)
+        print(dx,dy)
+        return angle([dx,dy], [1,0])
         
+        
+
      def moveProjectile(self):
         dx, dy = self.cibleX - self.rect.x, self.cibleY - self.rect.y
         dist = math.hypot(dx, dy)
-
-        dx, dy = dx / dist, dy / dist  # normalisation du vecteur
-        # bouger en direction du vecteur
-        self.rect.x += dx * self.velocity
-        self.rect.y += dy * self.velocity
+        if dist!=0:
+            dx, dy = dx / dist, dy / dist  # normalisation du vecteur
+            # bouger en direction du vecteur
+            self.rect.x += dx * self.velocity
+            self.rect.y += dy * self.velocity
         if self.rect.colliderect(self.cible.rect):
             self.cible.takeDamage(self.degat)
             self.kill()
             #detruire le projo
         
-        
-          
-     def mobDroite(self):
-        if self.deplacementAutorise("droite"):
-            self.goRight()
-            tuile = majSelectionJoueur(self.game, self.getFeet())
-            self.setPos(tuile)
-            return True
-        return False
-    
-     def mobGauche(self):
-        if self.deplacementAutorise("gauche"):
-            self.goLeft()
-            tuile = majSelectionJoueur(self.game, self.getFeet())
-            self.setPos(tuile)
-            return True
-        return False
-    
-     def mobBas(self):
-        if self.deplacementAutorise("bas"):
-            self.goDown()
-            tuile = majSelectionJoueur(self.game, self.getFeet())
-            self.setPos(tuile)
-            return True
-        return False        
-        
-     def mobHaut(self):
-        if self.deplacementAutorise("haut"):
-            self.goUp()
-            tuile = majSelectionJoueur(self.game, self.getFeet())
-            self.setPos(tuile)
-            return True
-        return False        
-         
-     def setPos(self, tuile):
-            self.posX, self.posY = tuile.posX, tuile.posY
 
-     def goLeft(self):
-            self.rect.x-=self.velocity
-     
-     
-        
-        
-     def goRight(self):
-        self.rect.x+=self.velocity
-        
-     def goUp(self):
-         self.rect.y-=self.velocity
-         
-     def goDown(self):
-         self.rect.y+=self.velocity
-         
+
+def produit(v1, v2):
+      return sum((a*b) for a, b in zip(v1, v2))
+
+def longueur(v):
+  return math.sqrt(produit(v, v))
+
+def angle(v1, v2):
+  return math.acos(produit(v1, v2) / (longueur(v1) * longueur(v2)))
