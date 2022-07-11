160f120308ddfdc36aa1dd66229bb7fd576a22a9
DedaleStudio <101816097+DedaleStudio@users.noreply.github.com>
2022-07-11 21:20:37 +0200

    Update tuile.py

--- tuile.py
+++ tuile.py
@@ -38,7 +38,7 @@ class Tuile(pygame.sprite.Sprite):
         
         
         self.estSelect = False
-        self.isExplored = True#self.type==7
+        self.isExplored = self.type==7
         
         ####     POSITION ET IMAGE    ####
         
