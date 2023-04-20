eec48f5655e4a0f30fe58923f3090e0829f1bfcc
Dédale <101816097+DedaleStudio@users.noreply.github.com>
2023-04-20 17:09:03 +0200

    Delete utils.py

--- utils.py
+++
@@ -1,8 +0,0 @@
-
-def convertToBool(chaine:str):
-    #fonction permettant la convertion d'une chaine de caractère str() vers un booléan True ou False
-    chaine =str(chaine)
-    if chaine =="True" or chaine == "true":
-        return True
-    else : 
-        return False
